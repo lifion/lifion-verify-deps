f0296d029deaf765b5e3433e64418fbd172ec501
Eckstein, Jenny (CORP) <Jenny.Eckstein@adp.com>
2020-01-16 12:19:51 -0500

    Added auto-upgrade option

--- bin/lifion-verify-deps.js
+++ bin/lifion-verify-deps.js
@@ -2,11 +2,28 @@
 
 'use strict';
 
+const argv = require('minimist')(process.argv.slice(2));
 const verifyDeps = require('../lib');
 
+if (argv.help) {
+  console.log(
+    [
+      '',
+      'Usage: lifion-verify-deps [options]',
+      '',
+      'Checks existing dependencies for available updates',
+      '',
+      'Options:',
+      '--help           Display this help message and exit',
+      '--auto_upgrade    Automatically run all suggested upgrades'
+    ].join('\n')
+  );
+}
+
 async function run() {
+  const { auto_upgrade: autoUpgrade } = argv;
   try {
-    await verifyDeps({ dir: process.cwd() });
+    await verifyDeps({ autoUpgrade, dir: process.cwd() });
     process.exit(0);
   } catch (err) {
     console.error(err.message);
